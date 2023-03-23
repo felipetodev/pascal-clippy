bddcad13873c596d47d64168b0e9d0468c5f5614
felipetodev <fe.ossandon.u@gmail.com>
2023-03-23 00:27:04 -0300

    add inter font jeee

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
+import { Inter } from 'next/font/google'
 import '@/styles/globals.css'
+import 'react-tooltip/dist/react-tooltip.css'
+
 import type { AppProps } from 'next/app'
 
+const inter = Inter({ subsets: ['latin'] })
+
 export default function App ({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <main className={inter.className}>
+    <Component {...pageProps} />
+  </main>
 }
