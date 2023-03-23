f3b590947aee34aff5a5b03ef79a72e4970b7210
felipetodev <fe.ossandon.u@gmail.com>
2023-03-23 00:27:22 -0300

    add lil seo (?)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,25 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document () {
+  const description = 'Instituto Pascal AI assistant - Powered by OpenAI'
+  const sitename = 'institutopascal.ai'
+  const title = 'Instituto Pascal AI assistant'
+  const ogimage = 'https://www.institutopascal.ai/og-image.png'
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="es" className='bg-clippy h-full'>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content={description} />
+        <meta property="og:site_name" content={sitename} />
+        <meta property="og:description" content={description} />
+        <meta property="og:title" content={title} />
+        <meta name="twitter:card" content="pascal_large_image" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={ogimage} />
+        <meta property="og:image" content={ogimage} />
+        <link href="https://unpkg.com/pattern.css" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
