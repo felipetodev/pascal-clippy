00f0dcd0d0dff4ff0e6b687a409a3c42fca8ebed
felipetodev <fe.ossandon.u@gmail.com>
2023-03-24 00:04:41 -0300

    add dropdown selector language

---
+++ components/DropDown.tsx
@@ -0,0 +1,79 @@
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+import {
+  CheckIcon,
+  ChevronDownIcon,
+  ChevronUpIcon
+} from '@heroicons/react/20/solid'
+
+import { type LangType } from '@/types'
+
+function classNames (...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
+
+interface DropDownProps {
+  language: LangType
+  setLanguage: (lang: LangType) => void
+}
+
+const langs: LangType[] = ['Español', 'Inglés', 'Chino', 'Ruso', 'Italiano']
+
+export default function DropDown ({ language, setLanguage }: DropDownProps) {
+  return (
+    <Menu as="div" className="relative block text-left w-full">
+      <div>
+        <Menu.Button className="font-medium inline-flex w-full justify-between items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500 ">
+          {language}
+          <ChevronUpIcon
+            className="-mr-1 ml-2 h-5 w-5 ui-open:hidden"
+            aria-hidden="true"
+          />
+          <ChevronDownIcon
+            className="-mr-1 ml-2 h-5 w-5 hidden ui-open:block"
+            aria-hidden="true"
+          />
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items
+          className="absolute left-0 z-10 mt-2 w-full origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+          key={language}
+        >
+          <div className="">
+            {langs.map((langItem) => (
+              <Menu.Item key={langItem}>
+                {({ active }) => (
+                  <button
+                    onClick={() => { setLanguage(langItem) }}
+                    className={classNames(
+                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                      language === langItem ? 'bg-gray-200' : '',
+                      'px-4 py-2 text-sm w-full text-left flex items-center space-x-2 justify-between'
+                    )}
+                  >
+                    <span>{langItem}</span>
+                    {language === langItem
+                      ? (
+                      <CheckIcon className="w-4 h-4 text-bold" />
+                        )
+                      : null}
+                  </button>
+                )}
+              </Menu.Item>
+            ))}
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  )
+}
