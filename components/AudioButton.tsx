ca559dfb369d5973ec56c97dc630a473760eb08f
felipetodev <fe.ossandon.u@gmail.com>
2023-03-24 02:07:38 -0300

    fix lint

--- components/AudioButton.tsx
+++ components/AudioButton.tsx
@@ -3,12 +3,12 @@ import { AudioRecorder } from 'react-audio-voice-recorder'
 export default function AudioButton ({ getWhisperResponse }: { getWhisperResponse: (blob: Blob) => void }) {
   return (
     <AudioRecorder
-    onRecordingComplete={async (blob) => getWhisperResponse(blob)}
-    classes={{
-      AudioRecorderStartSaveClass: 'audio-recorder-svg-color',
-      AudioRecorderPauseResumeClass: 'audio-recorder-svg-color',
-      AudioRecorderDiscardClass: 'audio-recorder-svg-color'
-    }}
-  />
+      onRecordingComplete={async (blob) => getWhisperResponse(blob)}
+      classes={{
+        AudioRecorderStartSaveClass: 'audio-recorder-svg-color',
+        AudioRecorderPauseResumeClass: 'audio-recorder-svg-color',
+        AudioRecorderDiscardClass: 'audio-recorder-svg-color'
+      }}
+    />
   )
 }
